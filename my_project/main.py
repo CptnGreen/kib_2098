5164cb10d2d9e122c207b16f80d6de4caaf1610a
CptnGreen <a.krasnoshchekov1992@gmail.com>
2019-10-21 14:18:43 +0300

    some explanations

--- my_project/main.py
+++ my_project/main.py
@@ -10,3 +10,50 @@ from pybricks.robotics import DriveBase
 
 # Write your program here
 brick.sound.beep()
+
+# создаём объекты класса Motor
+# для управления реальными моторами
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# для подачи тока на моторы (от -100 до 100)
+# используем метод dc объектов класса Motor
+
+# крутиться вправо 1 секунду:
+
+left_motor.dc(50)
+right_motor.dc(-50)
+wait(1000)
+
+# сочиняем функцию для поворота налево:
+
+def kib_turn_left():
+    left_motor.dc(50)
+    right_motor.dc(-50)
+    wait(1000)
+    left_motor.dc(0)
+    right_motor.dc(0)
+
+# вызываем её на исполнение:
+
+kib_turn_left()
+    
+# функция для поворота робота на 
+# произвольный неотрицательный угол 
+# (k - отладочный коэффициент):
+
+def kib_turn(angle):
+    k = 10
+    left_motor.dc(50)
+    right_motor.dc(-50)
+    wait(k * angle)
+    left_motor.dc(0)
+    right_motor.dc(0)
+
+kib_turn_left(360)
+kib_turn_left(720)
+kib_turn_left(90)
+
+
+    
